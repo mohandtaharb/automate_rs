ab3b8be1d03d272717a0f49300f49be6c43f3bd3
mohandtaharb <lemamouth2@gmail.com>
2023-05-15 15:56:57 +0100

    Allow user to input a word via stdin

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,12 @@
 pub mod automaton;
 
-use std::collections::{HashSet, HashMap};
-
+use std::{
+    collections::{HashSet, HashMap},
+    io
+};
 use automaton::*;
 
-fn main() {
+fn main() -> io::Result<()> {
     let transition_fn = HashMap::from([
         ((State::Zeros, '0'), State::Zeros),
         ((State::Zeros, 'b'), State::FirstB),
@@ -19,11 +21,14 @@ fn main() {
 
     let mut automate = Automaton::create(init_state, fstates, transition_fn);
 
-    let w = String::from("0bb1");
-
-    if automate.validate(&w) {
+    let mut word = String::new();
+    io::stdin().read_line(&mut word)?;
+    /* On supprime les espaces et retours à la ligne pour ne pas avoir de problème */
+    word = word.trim().to_string(); 
+    if automate.validate(&word) {
         println!("Le mot appartient au langage");
     } else {
         println!("Le mot n'appartient pas au langage");
     }
+    Ok(())
 }
