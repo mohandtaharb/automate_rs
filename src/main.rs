e251f28bfadaaf2280ed50a90f198db319a34b77
mohandtaharb <lemamouth2@gmail.com>
2023-05-17 19:59:50 +0100

    Add word read loop

--- src/main.rs
+++ src/main.rs
@@ -23,15 +23,18 @@ fn main() -> io::Result<()> {
     let mut automate = Automaton::create(init_state, fstates, transition_fn);
 
     let mut word = String::new();
-    print!("Entrez un mot : ");
-    io::stdout().flush().unwrap();
-    io::stdin().read_line(&mut word)?;
-    /* On supprime les espaces et retours à la ligne pour ne pas avoir de problème */
-    word = word.trim().to_string(); 
-    if automate.validate(&word) {
-        println!("Le mot appartient au langage");
-    } else {
-        println!("Le mot n'appartient pas au langage");
+    loop {
+        print!("Entrez un mot : ");
+        io::stdout().flush().unwrap();
+        io::stdin().read_line(&mut word)?;
+        /* On supprime les espaces et retours à la ligne pour ne pas avoir de problème */
+        word = word.trim().to_string(); 
+        if automate.validate(&word) {
+            println!("Le mot appartient au langage");
+        } else {
+            println!("Le mot n'appartient pas au langage");
+        }
+        word.clear();
     }
     Ok(())
 }
